be85f34ab171772f5dc2de11a44f5c2d86e4da88
中川真人 <masanew01@gmail.com>
2022-01-01 21:53:11 +0900

    ダイアログ表示を改行

--- app/src/main/java/com/example/a/loginDialogFragment.kt
+++ app/src/main/java/com/example/a/loginDialogFragment.kt
@@ -7,8 +7,12 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import com.example.a.Profile.ProfFragment
+import com.example.a.Security.Login.LoginFragment
 import com.example.a.Security.LoginActivity
+import kotlinx.coroutines.Dispatchers.Main
 import kotlin.math.log
 
 class loginDialogFragment: DialogFragment() {
@@ -17,16 +21,17 @@ class loginDialogFragment: DialogFragment() {
 
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            builder.setTitle("ああああああ")
+            builder.setTitle("この機能を利用するには\nユーザー登録が必要です")
                 .setItems(R.array.aiueo,
                     DialogInterface.OnClickListener { dialog, which ->
 
                         when(which) {
                             0 -> {
-                                Log.d(TAG, "ああ")
+                                startActivity(Intent(context,LoginFragment::class.java))
+                                Log.d(TAG, "登録する")
                             }
                             1 -> {
-                                Log.d(TAG, "いい")
+                                Log.d(TAG, "今はしない")
                             }
                         }
 

653602ec44f3fad61c6269ce60878e5f2ba1a25a
中川真人 <masanew01@gmail.com>
2022-02-24 21:14:14 +0900

    Adapter修正

---
+++ app/src/main/java/com/example/a/Post/CustomAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.a.Post
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a.R
+import org.w3c.dom.Text
+
+class CustomAdapter(private val postlist: ArrayList<Post>) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val icon: ImageView
+        val title: TextView
+        val post: TextView
+
+        init {
+            icon = view.findViewById(R.id.icon)
+            title = view.findViewById(R.id.tvtitle)
+            post = view.findViewById(R.id.tvpost)
+        }
+    }
+
+    //レイアウトの設定
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(viewGroup.context).inflate(R.layout.item_post, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    //Viewの設定
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val post = postlist[position]
+
+        viewHolder.title
+
+
+    }
+
+    //表示数を返す
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
